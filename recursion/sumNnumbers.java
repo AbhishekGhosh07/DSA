883626cc6f414448cdaa869a8d795dba31c55c8a
Abhishek Ghosh <abhi.ghosh3108@gmail.com>
2023-11-19 12:14:27 +0530

    without sum variable

--- recursion/sumNnumbers.java
+++ recursion/sumNnumbers.java
@@ -3,15 +3,12 @@ package recursion;
 import java.util.Scanner;
 
 public class sumNnumbers {
-    public static void printNames(int n,int sum){
+    public static int printNames(int n){
         if(n==0)
         {
-            System.out.println(sum);
-            return ;
+            return 0;
         }
-        sum = sum+n;
-
-         printNames(n-1,sum);
+        return n+ printNames(n-1);
 
 
 
@@ -19,6 +16,6 @@ public class sumNnumbers {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int sum=0;
-        printNames(scanner.nextInt(), sum);
+        System.out.println(printNames(scanner.nextInt()));
     }
 }
